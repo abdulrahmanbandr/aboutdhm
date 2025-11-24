f0e45eee01e6f1833d60d869d71d1cfdc387c859
d <f>
2025-11-24 20:59:08 +0300

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,20 +11,20 @@ export default function AboutPage() {
             <div className="hero">
                 <h1>About Me</h1>
                 <p>
-                    Hi, I'm Abdulrahman aldahhas . I'm a software engineer passionate about building clean, efficient, and user-centric applications.
+                    Hi Manal, I'm Abdulrahman aldahhas, a cloud engineer and much more! .
                 </p>
                 <p>
-                    I believe in the power of minimalism—stripping away the unnecessary to focus on what truly matters. This philosophy reflects in my code, my designs, and my lifestyle.
+
                 </p>
             </div>
 
             <div className="journey">
                 <h2>My Journey</h2>
                 <p>
-                    My journey into tech started with a curiosity about how things work. Over the years, I've honed my skills in full-stack development, exploring various technologies and frameworks.
+                    stay tuned !
                 </p>
                 <p>
-                    Currently, I'm focused on Next.js and the React ecosystem, constantly learning and experimenting with new ideas.
+                    stay tuned ! .
                 </p>
             </div>
         </section>
